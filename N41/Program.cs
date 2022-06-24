68b5855579baadc7431792872470025c7d9c9458
Lena <len.dal@yandex.ru>
2022-06-24 14:21:42 +0300

    first task

---
+++ N41/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введи числа: ");
+int[] number = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);;
+int up = 0;
+
+
+for (int i = 0; i < number.Length; i++)
+{
+    if (number[i] > 0)
+    {
+        up++;
+    }
+}
+ 
+Console.WriteLine($"Чисел больше нуля: {up}");
